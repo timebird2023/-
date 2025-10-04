05e97fd9169e947cf6ad2d8d55b7fb47155aaba7
timebird2023 <timebird2023@gmail.com>
2025-10-04 19:50:04 +0100

    Create pinger.js

---
+++ api/pinger.js
@@ -0,0 +1,9 @@
+{
+  "name": "replit-keep-alive-pinger",
+  "version": "1.0.0",
+  "description": "A Vercel Serverless Function to keep a Replit project active.",
+  "dependencies": {
+    "node-fetch": "2.6.7" 
+    // نستخدم إصداراً قديماً وثابتاً لضمان التوافق مع بيئة Vercel
+  }
+}
